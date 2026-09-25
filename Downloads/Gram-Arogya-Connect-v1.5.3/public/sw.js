39310967669ce453dd89ed9a162b93a7062b97ab
Apoorv Mishra <apoorvmishra2007@gmail.com>
2026-09-25 20:10:10 +0530

    Delete Downloads/Gram-Arogya-Connect-v1.5.3 directory

--- Downloads/Gram-Arogya-Connect-v1.5.3/public/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='gac-v1.5.2'; const ASSETS=['/','/index.html','/css/style.css','/js/app.js','/images/sunita-tai.webp','/manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/'))return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))))});
